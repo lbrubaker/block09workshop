7e5a808e6cd9d1999959c46a76e76c70944ba72c
Lauren Brubaker <ldelisle@creditacceptance.com>
2024-07-16 10:25:08 -0400

    July 16 changes

---
+++ personal_site/javascript.js
@@ -0,0 +1,13 @@
+const homePage = "Hello World! Welcome to my Home Page!"
+
+console.log(homePage); 
+
+
+const aboutPage = "Hello World! Welcome to my About Page!"
+
+console.log(aboutPage); 
+
+
+const portfolioPage = "Hello World! Welcome to my Portfolio Page!"
+
+console.log(portfolioPage); 
